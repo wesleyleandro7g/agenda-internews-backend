1bd494b0bbe7a77988e66ad444ecbd8b0f979a2e
wesleyleandro7g <wesleyleandro.dev@gmail.com>
2021-03-24 13:11:30 -0300

    Criado relatorio de atendimentos

--- src/controllers/ReportAttendences.js
+++ src/controllers/ReportAttendences.js
@@ -1,12 +1,13 @@
 const Atendimentos = require('../models/Atendimentos')
 const AtividadeInterna = require('../models/AtividadeInterna')
 const Clientes = require('../models/Clientes')
+const Suporte = require('../models/Suporte')
 const { Op } = require('sequelize')
 
 module.exports = {
-    async unattendedCostumer(req, res) {
+    async unattendedCostumerAll(req, res) {
         try {
-			const { id_suporte, initial_date, finish_date } = req.body
+			const { initial_date, finish_date } = req.body
 			
             const internalActivitiesValid = await AtividadeInterna.findAll({
 				attributes: ['id'],
@@ -36,8 +37,8 @@ module.exports = {
 				where: {
 					id_status: 4,
 					created_at: {
-						[Op.gte]: firstDay,
-						[Op.lte]: lastDay
+						[Op.gte]: initial_date ? initial_date : firstDay,
+						[Op.lte]: finish_date ? finish_date : lastDay
 					}
 				}
 			})
@@ -59,7 +60,71 @@ module.exports = {
 				where: {
 					id: {
 						[Op.not]: [...uniqueValue]
+					},
+					id_atividade_interna: [...validActivitiesID]
+				}
+			})
+
+			return res.status(200).json({ unattendedCostumer })
+        } catch (error) {
+            return res.status(500).json({ error: error });
+        }
+    },
+	async costumersServedAll(req, res) {
+        try {
+			const { initial_date, finish_date } = req.body
+			
+            const internalActivitiesValid = await AtividadeInterna.findAll({
+				attributes: ['id'],
+				where: {
+					desconsiderar_relatorio: false
+				}
+			})
+
+			const validActivitiesID = []
+			internalActivitiesValid.map(item => {
+				validActivitiesID.push(item.id)
+			})
+
+			const { count, rows: clients} = await Clientes.findAndCountAll({
+				attributes: [ 'id', 'nome' ],
+				where: {
+					id_atividade_interna: [...validActivitiesID]
+				}
+			})
+			
+			const date = new Date()
+			const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+			const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+			
+			const attendences = await Atendimentos.findAll({
+				attributes: [ 'id', 'createdAt', 'id_cliente' ],
+				where: {
+					id_status: 4,
+					created_at: {
+						[Op.gte]: initial_date ? initial_date : firstDay,
+						[Op.lte]: finish_date ? finish_date : lastDay
 					}
+				}
+			})
+			
+			const Object = []
+			
+			clients.map(client => {
+				attendences.map(attendence => {
+					if(attendence.id_cliente === client.id){
+						Object.push(client.id)
+					}
+				})
+			})
+			
+			const uniqueValue = [...new Set(Object)]
+
+			const unattendedCostumer = await Clientes.findAll({
+				attributes: ['id', 'nome'],
+				where: {
+					id: [...uniqueValue],
+					id_atividade_interna: [...validActivitiesID]
 				}
 			})
 
@@ -70,7 +135,16 @@ module.exports = {
     },
 	async unattendedCostumerPerSupport(req, res) {
         try {
-			const { id_suporte, initial_date, finish_date } = req.body
+			const { initial_date, finish_date } = req.body
+			const { id_usuario } = req.headers
+
+			const suporte = await Suporte.findOne({
+				where: {
+					id_usuario
+				}
+			})
+			
+			if(!suporte) return res.status(404).json({ message: 'Suporte não encontrado!' })
 
             const internalActivitiesValid = await AtividadeInterna.findAll({
 				attributes: ['id'],
@@ -88,7 +162,7 @@ module.exports = {
 				attributes: [ 'id', 'nome' ],
 				where: {
 					id_atividade_interna: [...validActivitiesID],
-					id_suporte
+					id_suporte: suporte.id
 				}
 			})
 			
@@ -101,8 +175,8 @@ module.exports = {
 				where: {
 					id_status: 4,
 					created_at: {
-						[Op.gte]: firstDay,
-						[Op.lte]: lastDay
+						[Op.gte]: initial_date ? initial_date : firstDay,
+						[Op.lte]: finish_date ? finish_date : lastDay
 					}
 				}
 			})
@@ -124,8 +198,90 @@ module.exports = {
 				where: {
 					id: {
 						[Op.not]: [...uniqueValue]
+					},
+					id_atividade_interna: [...validActivitiesID],
+					id_suporte: suporte.id
+				},
+				include: [
+					{ association: 'suporte' }
+				]
+			})
+
+			return res.status(200).json({ unattendedCostumer })
+        } catch (error) {
+            return res.status(500).json({ error: error });
+        }
+    },
+	async costumersServedPerSupport(req, res) {
+        try {
+			const { initial_date, finish_date } = req.body
+			const { id_usuario } = req.headers
+
+			const suporte = await Suporte.findOne({
+				where: {
+					id_usuario
+				}
+			})
+			
+			if(!suporte) return res.status(404).json({ message: 'Suporte não encontrado!' })
+
+            const internalActivitiesValid = await AtividadeInterna.findAll({
+				attributes: ['id'],
+				where: {
+					desconsiderar_relatorio: false
+				}
+			})
+
+			const validActivitiesID = []
+			internalActivitiesValid.map(item => {
+				validActivitiesID.push(item.id)
+			})
+
+			const clients = await Clientes.findAll({
+				attributes: [ 'id', 'nome' ],
+				where: {
+					id_atividade_interna: [...validActivitiesID],
+					id_suporte: suporte.id
+				}
+			})
+			
+			const date = new Date()
+			const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+			const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+			
+			const attendences = await Atendimentos.findAll({
+				attributes: [ 'id', 'createdAt', 'id_cliente' ],
+				where: {
+					id_status: 4,
+					created_at: {
+						[Op.gte]: initial_date ? initial_date : firstDay,
+						[Op.lte]: finish_date ? finish_date : lastDay
 					}
 				}
+			})
+			
+			const Object = []
+			
+			clients.map(client => {
+				attendences.map(attendence => {
+					if(attendence.id_cliente === client.id){
+						Object.push(client.id)
+					}
+				})
+			})
+			
+			const uniqueValue = [...new Set(Object)]
+
+			const unattendedCostumer = await Clientes.findAll({
+				attributes: ['id', 'nome'],
+				where: {
+					id: [...uniqueValue],
+					id_atividade_interna: [...validActivitiesID],
+					id_suporte: suporte.id
+				},
+				include: [
+					{ association: 'suporte' }
+				]
 			})
 
 			return res.status(200).json({ unattendedCostumer })
--- src/routes/ReportRoutes.js
+++ src/routes/ReportRoutes.js
@@ -3,7 +3,9 @@ const router = routes();
 
 const ReportController =  require('../controllers/ReportAttendences')
 
-router.get('/index', ReportController.unattendedCostumer)
-router.get('/support', ReportController.unattendedCostumerPerSupport)
+router.post('/index', ReportController.unattendedCostumerAll)
+router.post('/index-served', ReportController.costumersServedAll)
+router.post('/support', ReportController.unattendedCostumerPerSupport)
+router.post('/support-served', ReportController.costumersServedPerSupport)
 
 module.exports = router
